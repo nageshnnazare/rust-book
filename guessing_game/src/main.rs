6c2ae4d668d4e1ee0935da68dc9df7677c7fe40e
Nagesh Nazare <nageshnnazare@gmail.com>
2023-04-14 22:16:20 +0530

    Added colors to the guessing game output

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
@@ -27,10 +28,10 @@ fn main() {
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
             Ordering::Equal => {
-                println!("You win!");
+                println!("{}", "You win!".green());
                 break;
             }
         }
